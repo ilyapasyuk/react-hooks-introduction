6cc49c77510fba6124c0e7f217d2c33ebb26404a
ilyapasyuk <pasyuk.com@gmail.com>
2019-06-21 22:03:26 +0300

    use "useState" hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,51 +1,40 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Scroller from '../Scroller'
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+const App = ({}) => {
+    document.title = 'Homepage';
 
-        this.state = {
-            isShowModal: false
-        }
-    }
+    const [isShowModal, setModal] = useState(false);
 
-    componentDidMount() {
-        document.title = 'Homepage'
-    }
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello world</h1>
-
-                <button onClick={() => this.setState({isShowModal: true})}>show Scroller</button>
+            <button onClick={() => setModal(true)}>show Scroller</button>
 
 
-                {this.state.isShowModal && (
-                    <Scroller onClose={() => this.setState({isShowModal: false})}>
-                        {['', '', '', '', '', '', ''].map((i, idx) => (
-                            <p key={idx}>
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                                has
-                                been the industry's standard dummy text ever since the 1500s, when an unknown printer
-                                took a
-                                galley of type and scrambled it to make a type specimen book. It has survived not only
-                                five
-                                centuries, but also the leap into electronic typesetting, remaining essentially
-                                unchanged.
-                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem
-                                Ipsum
-                                passages, and more recently with desktop publishing software like Aldus PageMaker
-                                including
-                                versions of Lorem Ipsum.
-                            </p>))}
-                    </Scroller>
-                )}
-            </div>
-        )
-    }
-
-}
+            {isShowModal && (
+                <Scroller onClose={() => setModal(false)}>
+                    {['', '', '', '', '', '', ''].map((i, idx) => (
+                        <p key={idx}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                            has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                            took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only
+                            five
+                            centuries, but also the leap into electronic typesetting, remaining essentially
+                            unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem
+                            Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker
+                            including
+                            versions of Lorem Ipsum.
+                        </p>))}
+                </Scroller>
+            )}
+        </div>
+    )
+};
 
 export default App;
