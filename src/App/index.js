577fccb54a189170a5de27396c8748268d9a048d
ilyapasyuk <pasyuk.com@gmail.com>
2019-11-06 22:14:34 +0300

    lesson four

--- src/App/index.js
+++ src/App/index.js
@@ -1,38 +1,18 @@
-import React, {useState} from 'react';
-import Scroller from '../Scroller'
+import React, {useState, useRef} from 'react';
 
-const App = ({}) => {
-    document.title = 'Homepage';
+const App = () => {
+    const [value, setValue] = useState('');
+    const fieldRef = useRef(null);
 
-    const [isShowModal, setModal] = useState(false);
+    function fieldHandler() {
+        setValue(fieldRef.current.value)
+    }
 
     return (
         <div className="App">
-            <h1>Hello world</h1>
-
-            <button onClick={() => setModal(true)}>show Scroller</button>
-
-
-            {isShowModal && (
-                <Scroller onClose={() => setModal(false)}>
-                    {['', '', '', '', '', '', ''].map((i, idx) => (
-                        <p key={idx}>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                            has
-                            been the industry's standard dummy text ever since the 1500s, when an unknown printer
-                            took a
-                            galley of type and scrambled it to make a type specimen book. It has survived not only
-                            five
-                            centuries, but also the leap into electronic typesetting, remaining essentially
-                            unchanged.
-                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem
-                            Ipsum
-                            passages, and more recently with desktop publishing software like Aldus PageMaker
-                            including
-                            versions of Lorem Ipsum.
-                        </p>))}
-                </Scroller>
-            )}
+            {value && <p>{value}</p>}
+            <input type="text" ref={fieldRef}/>
+            <button onClick={() => fieldHandler()}>Show value</button>
         </div>
     )
 };
